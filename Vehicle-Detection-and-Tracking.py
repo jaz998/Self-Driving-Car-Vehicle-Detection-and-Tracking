acf225323895f338e7d785d01df5c6034d5c80e6
jaz998 <jaz998@users.noreply.github.com>
2018-07-08 22:37:25 +1000

    heat map done

--- Vehicle-Detection-and-Tracking.py
+++ Vehicle-Detection-and-Tracking.py
@@ -9,6 +9,43 @@ import time
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
+from scipy.ndimage.measurements import label
+
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap  # Iterate through list of bboxes
+
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1] + 1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        print('bbox[0]', bbox[0])
+        print('bbox[1]', bbox[1])
+        cv2.rectangle(img, bbox[0], bbox[1], (0, 0, 255), 6)
+    # Return the image
+    return img
 
 
 def convert_color(img, conv='RGB2YCrCb'):
@@ -22,6 +59,8 @@ def convert_color(img, conv='RGB2YCrCb'):
 
 # Define a single function that can extract features using hog sub-sampling and make predictions
 def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+    coordinates_list = []
+
     draw_img = np.copy(img)
     img = img.astype(np.float32) / 255
 
@@ -85,8 +124,10 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
                 win_draw = np.int(window * scale)
                 cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
                               (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+                coordinates = ((xbox_left, (ytop_draw + ystart)), ((xbox_left + win_draw), (ytop_draw + win_draw + ystart)))
+                coordinates_list.append(coordinates)
 
-    return draw_img
+    return draw_img, coordinates_list
 
 # Define a function that takes an image,
 # start and stop positions in both x and y,
@@ -375,7 +416,7 @@ for image in images:
 
 
 # Use a smaller sample for testing hog features classifier
-sample_size = 500
+sample_size = 2000
 hog_cars = cars[0:sample_size]
 hog_notcars = notcars[0:sample_size]
 
@@ -494,9 +535,35 @@ ystart = 400
 ystop = 656
 scale = 1.5
 
-out_img = find_cars(image2, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial, histbin)
+out_img, coordinates_list = find_cars(image2, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial, histbin)
 plt.imshow(out_img)
 plt.show()
+
+heat = np.zeros_like(image2[:,:,0]).astype(np.float)
+
+# Add heat to each box in box list
+heat = add_heat(heat, coordinates_list)
+
+# Apply threshold to help remove false positives
+heat = apply_threshold(heat, 1)
+
+# Visualize the heatmap when displaying
+heatmap = np.clip(heat, 0, 255)
+
+# Find final boxes from heatmap using label function
+labels = label(heatmap)
+draw_img = draw_labeled_bboxes(np.copy(image2), labels)
+
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(draw_img)
+plt.title('Car Positions')
+plt.subplot(122)
+plt.imshow(heatmap, cmap='hot')
+plt.title('Heat Map')
+fig.tight_layout()
+plt.show()
+
 
 
 
